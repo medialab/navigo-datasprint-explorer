30d7afee3fc98ef27f997accdd39edcd4b1ba1fa
Guillaume Brioudes <guillaume.brioudes@myllaume.fr>
2022-02-21 17:54:44 +0100

    [fix] deploy

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+mkdir data
 echo "fetching latest navigo pointcalls data"
 curl -o data/navigo_all_pointcalls_1789.csv "data.portic.fr/api/pointcalls/?date=1789&format=csv"
 curl -o data/navigo_all_pointcalls_1787.csv "data.portic.fr/api/pointcalls/?date=1787&format=csv"
